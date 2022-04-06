e9ec5450940bd576f43c4ed07f17ae228dbd0eb8
Breno <brenobeirigo@gmail.com>
2022-04-06 14:36:12 +0200

    cleanup

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='vrplot',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Plot VRP routes',
+    author='Breno A. Beirigo',
+    license='MIT',
+)
